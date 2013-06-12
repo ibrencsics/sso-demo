bc54f10e327e8aa733e87186989821205f191a30
ivan <ivan.brencsics@gmail.com>
2013-06-12 14:33:05 +0200

    cleanup

--- sso-demo-comm/sso-demo-comm-service/src/test/java/org/ib/sso/comm/sts/service1/Service1Test.java
+++ sso-demo-comm/sso-demo-comm-service/src/test/java/org/ib/sso/comm/sts/service1/Service1Test.java
@@ -1,6 +1,5 @@
 package org.ib.sso.comm.sts.service1;
 
-import org.apache.hello_world_soap_http.Greeter;
 import org.ib.sso.service.service1.Service1Endpoint;
 import org.ib.sso.xsd.TestRequestType;
 import org.ib.sso.xsd.TestResponseType;
@@ -15,16 +14,13 @@ public class Service1Test {
 	
 	public ApplicationContext getContext() {
 		if (ctx==null) {
-			ctx = new ClassPathXmlApplicationContext("classpath:org/ib/sso/comm/sts/service1/applicationContext.xml");
+			ctx = new ClassPathXmlApplicationContext("classpath:org/ib/sso/comm/sts/service1/cxf-client.xml");
 		}
         return ctx;
     }
 	
 	@Test
 	public void callTest() {
-//		Greeter service = (Greeter) getContext().getBean("HelloServiceClient");
-//		String reply = service.greetMe();
-//		System.out.println("Received: " + reply);
 		
 		Service1Endpoint service = (Service1Endpoint) getContext().getBean("Service1Client");
 		TestRequestType request = new TestRequestType();
