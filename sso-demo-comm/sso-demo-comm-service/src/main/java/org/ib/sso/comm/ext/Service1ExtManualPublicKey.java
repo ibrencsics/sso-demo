5a48748d84992151b43cfb9690b8f127b534093d
Ivan Brencsics <ivan.brencsics@gmail.com>
2013-08-07 00:30:16 +0200

    Service2 wired

--- sso-demo-comm/sso-demo-comm-service/src/main/java/org/ib/sso/comm/ext/Service1ExtManualPublicKey.java
+++
@@ -1,5 +0,0 @@
-package org.ib.sso.comm.ext;
-
-public class Service1ExtManualPublicKey {
-
-}
--- sso-demo-comm/sso-demo-comm-service/src/main/java/org/ib/sso/comm/in/Service2Int.java
+++ sso-demo-comm/sso-demo-comm-service/src/main/java/org/ib/sso/comm/in/Service2Int.java
@@ -1,7 +1,15 @@
 package org.ib.sso.comm.in;
 
+import javax.annotation.Resource;
+import javax.xml.ws.WebServiceContext;
+
+import org.ib.sso.comm.lib.CommLibException;
+import org.ib.sso.comm.lib.SamlTokenCallbackHandler;
+import org.ib.sso.comm.lib.security.SAMLData;
+import org.ib.sso.comm.lib.security.WebServiceContextTool;
 import org.ib.sso.service.service2.Service2Endpoint;
 import org.ib.sso.service.service2.TestOperationFault;
+import org.ib.sso.xsd.TestFaultType;
 import org.ib.sso.xsd.TestRequestType;
 import org.ib.sso.xsd.TestResponseType;
 import org.slf4j.Logger;
@@ -16,12 +24,15 @@ public class Service2Int implements Service2Endpoint, ApplicationContextAware {
 	
 	private ApplicationContext appCtx;
 	
+	@Resource
+    WebServiceContext context;
+	
 	@Override
 	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
 		this.appCtx = applicationContext;
 	}
 	
-	private final boolean stopHere = true;
+	private final boolean stopHere = false;
 	
 	public TestResponseType testOperation(TestRequestType request)	throws TestOperationFault {
 
@@ -35,16 +46,41 @@ public class Service2Int implements Service2Endpoint, ApplicationContextAware {
 		if (stopHere) {
 			return response;
 		}
-		
-		
-		// call Service2
-		
-		response.getNode().add("COMM >>> Call Service2");
-		
-		Service2Endpoint service2Client = (Service2Endpoint) appCtx.getBean("service2Client");
-		TestResponseType service2Response = service2Client.testOperation(request);
-		
-		response.getNode().addAll(service2Response.getNode());
+
+
+		try {
+			// set token
+			
+			SAMLData samlData = WebServiceContextTool.getSamlData(context);
+			
+			SamlTokenCallbackHandler cb = (SamlTokenCallbackHandler) appCtx.getBean("samlTokenCallbackHandler");
+			cb.setToken(samlData.getToken());
+			
+			// call Service2
+			
+			response.getNode().add("COMM >>> Call Service2");
+			
+			Service2Endpoint service2Client = (Service2Endpoint) appCtx.getBean("service2BearerManualClient");
+			TestResponseType service2Response = service2Client.testOperation(request);
+			
+			response.getNode().addAll(service2Response.getNode());
+			
+		} catch (CommLibException e) {
+			e.printStackTrace();
+			
+			TestFaultType testFaultType = new TestFaultType();
+			testFaultType.setType("Business");
+			testFaultType.setDescription(e.getMessage());
+			throw new TestOperationFault(e.getMessage(), testFaultType);
+			
+		} catch (org.ib.sso.service.service2.TestOperationFault e) {
+			e.printStackTrace();
+			
+			TestFaultType testFaultType = new TestFaultType();
+			testFaultType.setType("Business");
+			testFaultType.setDescription(e.getMessage());
+			throw new TestOperationFault(e.getMessage(), testFaultType);
+		}
 		
 		return response;
 	}
